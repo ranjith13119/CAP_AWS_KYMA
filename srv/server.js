34f54438632579e7d42e5032e751c657d23fb85d
Ranjithkumar <30929584+ranjith13119@users.noreply.github.com>
2022-06-13 14:40:06 +0530

    Update server.js

--- srv/server.js
+++ srv/server.js
@@ -4,31 +4,25 @@ const cds = require("@sap/cds");
 const helmet = require("helmet");
 const proxy = require("@sap/cds-odata-v2-adapter-proxy");
 const cdsSwagger = require("cds-swagger-ui-express");
-var passport = require('passport');
-var xsenv = require('@sap/xsenv');
-var JWTStrategy = require('@sap/xssec').JWTStrategy;
-var services = xsenv.getServices({ uaa: { tag: 'xsuaa' } }); //Get the XSUAA service
-passport.use(new JWTStrategy(services.uaa));
-cds.on("bootstrap", (app) =>
-    app.use(
-        helmet({
-            // Creating a Content Security Policy (CSP) is a major building block in securing your web application.
-            contentSecurityPolicy: {
-                directives: {
-                    ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-                },
-            },
-        }),
-        proxy(),
-        cdsSwagger({
-            //Exposing the service as a Swagger api
-            basePath: "/swagger",
-            diagram: "true",
-        }),
-        passport.initialize(),
-        passport.authenticate('JWT', { session: false })
-    )
 
+cds.on("bootstrap", (app) =>
+  app.use(
+    helmet({
+      // Creating a Content Security Policy (CSP) is a major building block in securing your web application.
+      contentSecurityPolicy: {
+        directives: {
+          ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        },
+      },
+    }),
+    proxy(),
+    cdsSwagger({
+      //Exposing the service as a Swagger api
+      basePath: "/swagger",
+      diagram: "true",
+    })
+  )
+ 
 );
 
 module.exports = cds.server;
